573d35acf4f40d22bbaa24c322c51702a6af4eb6
Shougo Matsushita <Shougo.Matsu@gmail.com>
2023-12-25 13:05:54 +0900

    Update deps

--- denops/@ddc-filters/matcher_vimregexp.ts
+++ denops/@ddc-filters/matcher_vimregexp.ts
@@ -2,8 +2,8 @@ import {
   BaseFilter,
   Item,
   SourceOptions,
-} from "https://deno.land/x/ddc_vim@v3.4.0/types.ts";
-import { Denops, fn } from "https://deno.land/x/ddc_vim@v3.4.0/deps.ts";
+} from "https://deno.land/x/ddc_vim@v4.3.1/types.ts";
+import { Denops, fn } from "https://deno.land/x/ddc_vim@v4.3.1/deps.ts";
 
 type Params = {
   maxMatchLength: number;
